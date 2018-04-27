47ff29e4bd8aafab2394480abb770c551b090ccc
Felipe Carlos Campelo <fcampelocc@gmail.com>
2018-04-26 22:26:13 -0300

    .

---
+++ src/main.js
@@ -0,0 +1,7 @@
+class Init {
+  constructor () {
+    console.log('Ola mundo ')
+  }
+}
+
+new Init();
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: path.resolve(__dirname, 'dist/index.html')
+    })
+  ]
+}
